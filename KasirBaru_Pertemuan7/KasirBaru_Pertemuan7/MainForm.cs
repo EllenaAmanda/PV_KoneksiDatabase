55c867d148632fdd50f0d2d297871353857b1bb7
EllenaAmanda <76916918+EllenaAmanda@users.noreply.github.com>
2022-03-31 11:49:31 +0700

    Add files via upload

---
+++ KasirBaru_Pertemuan7/KasirBaru_Pertemuan7/MainForm.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: USER
+ * Date: 31/03/2022
+ * Time: 10.47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace KasirBaru_Pertemuan7
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		private SqlCommand cmd;
+		private DataSet ds;
+		private SqlDataAdapter da;
+		
+		public MainForm()
+		{
+			
+			InitializeComponent();
+			
+		}
+		
+		koneksi Konn = new koneksi();
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			SqlConnection conn = Konn.GetConn();
+			try
+			{
+				conn.Open();
+				cmd = new SqlCommand("Select * from TBL_BARANG", conn);
+				ds = new DataSet();
+				da = new SqlDataAdapter(cmd);
+				da.Fill(ds, "TBL_BARANG");
+				dataGridView1.DataSource = ds;
+				dataGridView1.DataMember = "TBL_BARANG";
+				dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			}
+			catch (Exception G)
+			{
+				MessageBox.Show(G.ToString());
+			}
+			finally
+			{
+				conn.Close();
+			}
+		}
+	}
+}
